58011966e09130b102e9ff4c2696731d685822d4
wungjyan <1170017412@qq.com>
2021-03-22 09:43:24 +0800

    add some

---
+++ src/utils/request.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const service = axios.create({
+    baseURL:'https://cnodejs.org/api/v1',
+    timeout:5000
+})
+
+export default service
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import { AxiosResponse, AxiosError } from "axios";
+import { getTopics } from "@/api/topics";
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    const obj = { page: 1, tab: "ask", limit: 10 };
+    getTopics(obj)
+      .then((res: AxiosResponse): void => {
+        console.log(res);
+      })
+      .catch((err: AxiosError): void => {
+        console.log(err);
+      });
   },
 });
 </script>
